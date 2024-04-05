069a8138c80a4d8023b88b6278cdcc04730234f1
Najjahi <i.najjihi@hotmail.com>
2024-04-05 16:45:16 +0200

    05/04

--- 03_JavaScript/03_projets/ilovejs/assets/js/script.js
+++ 03_JavaScript/03_projets/ilovejs/assets/js/script.js
@@ -1,18 +1,14 @@
-//----------------dark mode------------------------//
+// --------------------- Dark mode -----------------//
 
-let switchBox = document.querySelector('.switch');
+let switchBox = document.querySelector('.switch'); // je stock l'élément div.switch
 
-let container = document.querySelector('.container-fluid');
+let container = document.querySelector('.container-fluid'); // je stock l'élément div.container
 
-// je stock l'element div .container
-
-switchBox.addEventListener('click', changeMode);
-
- // je declare l'evenement cick sur la div.switch
+switchBox.addEventListener('click', changeMode);// je déclare l'événement click sur la div.switch 
 
 let nav = document.querySelector('.navbar-desktop');
 
-let liens = document.querySelectorAll('.navbar-desktop a')
+let liens = document.querySelectorAll('.navbar-desktop a');
 
 let btnChange = document.querySelector('.btn');
 
@@ -20,31 +16,21 @@ let icon = document.querySelector('.switch i');
 
 let titre = document.querySelector('h1');
 
-let btnsmile = document.querySelector('bloc-btn i')
 
+function changeMode(){
 
-
-
-function changeMode() {
-
-    // la div container change de couleur de fond   
-    
+    //  la div container ch&nge de couleur de fond
+    container.classList.toggle('container-change');
     nav.classList.toggle('nav-dark');
-   
-    container.classList.toggle('container-change'); // toggele = basculer
-   
-    for (const lien of liens) {
-       
-        lien.classList.toggle('lienDarkMode');
+
+    for (let lien of liens) {
         
+        lien.classList.toggle('lienDarkMode');
     }
 
-    // je déplace le bouton
-
+    // je déplace le bouton 
     btnChange.classList.toggle('btn-change');
-
-// je change la couleur du bouton
-
+    //je change la couleur du bouton
     btnChange.classList.toggle('btn-dark');
 
     icon.classList.toggle('icone-change');
@@ -52,38 +38,74 @@ function changeMode() {
     if (icon.classList.contains('bi-sun-fill')) {
 
         icon.classList.remove('bi-sun-fill');
-
-        icon.classList.add('bi-moon-fill');
+        icon.classList.add("bi-moon-fill");
         
     } else {
-
         icon.classList.remove('bi-moon-fill');
-
-        icon.classList.add('bi-sun-fill');        
-        
+        icon.classList.add("bi-sun-fill");  
     }
-        switchBox.classList.toggle('switch-change');
 
-        titre.classList.toggle('titre');
+    switchBox.classList.toggle('switch-change');
 
-        btnChange.classList.toggle('.btn-add')
+    titre.classList.toggle('titre');
 
-    if (btnChange.classList.contains('bg-black')) {
+}
 
-        btnChange.classList.remove('bg-black');
+//-----------------------partie video-----------------------//
+// -----------------------icone smile-------------------//
 
-        btnChange.classList.add('btn-add');
+
+let iconeSmile = document.querySelector('.bi-emoji-neutral');
+
+iconeSmile.addEventListener('click', ()=> {
+    
+    // 1 ere methode
+
+    if (iconeSmile.classList.contains('bi-emoji-neutral')) {
+
+        iconeSmile.classList.remove('bi-emoji-neutral');
+        iconeSmile.classList.add("bi-emoji-heart-eyes-fill");
         
     } else {
-
-        btnChange.classList.remove('btn-add');
-
-        btnChange.classList.add('bg-black');        
-        
-    }
-        // icon.classList.toggle('iconer')
-    
+        iconeSmile.classList.remove('bi-emoji-heart-eyes-fill');
+        iconeSmile.classList.add("bi-emoji-neutral");  
+    };
 }
+    )
+
+     // 2 eme methode
+
+    //  iconeSmile.classList.toggle('bi-emoji-wink-fill');
+
+     //-----bouton abonez vous-----//
+
+     let btnAbonne = document.querySelector('.btn-abonner');
+     
+     btnAbonne.addEventListener('click',() =>{
+
+        if (btnAbonne.innerText === "Abonnez-vous") {
+            btnAbonne.innerHTML = 'Abonné <i class="bi bi-check"></i>';
+            
+        } else {
+            btnAbonne.innerText = 'Abonnez-vous';
+        }
+        })
+    
+        //---------------------cookies----------------------//
+
+        let btnCookies = document.querySelector('.btn-success');3
+        
+        let cookies = document.querySelector('.cookies');
+
+        btnCookies.addEventListener('click', ()=> {
+        
+            // cookies.style.opacity = "0";
+            cookies.style.bottom = "auto";
+
+            
+
+        })
+
 
 
 
